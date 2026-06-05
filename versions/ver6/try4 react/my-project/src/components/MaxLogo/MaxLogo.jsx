4bb75a04f5a7636413c98a9f8499ecf439fa2db8
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-05 13:36:42 +0300

    Finally, a logo without a lantern, but it needs centering.

--- versions/ver6/try4 react/my-project/src/components/MaxLogo/MaxLogo.jsx
+++ versions/ver6/try4 react/my-project/src/components/MaxLogo/MaxLogo.jsx
@@ -1,49 +1,31 @@
 import React from 'react'
-
-import "./MaxLogo.css"  // ✅ точка указывает на текущую директорию
+import "./MaxLogo.css"
 import MaxLogoSvg from '../../../public/Max_logo.svg'
-
 
 const MaxLogo = () => {
   return (
     <div>
-                        <div className="joint3-thq-logostelegram-elm2">
-                          <img alt="Vector7286" src="public/vector7286-15ru.svg" className="joint3-thq-vector-elm5" />
-                          <div className="joint3-thq-maxmessengerhorizontallogo1-elm">
-                            <div className="joint3-thq-group-elm6">
-                              <div className="joint3-thq-clippathgroup-elm1">
-                                <div className="joint3-thq-svgid000000412575222465570809300000162176433937619-elm">
-                                  <img alt="Vector7287" src="public/vector7287-vj3r.svg" className="joint3-thq-vector-elm6" />
-                                </div>
-                                <div className="joint3-thq-group-elm7">
-                                  <div className="joint3-thq-clippathgroup-elm2">
-                                    <div className="joint3-thq-svgid000000275732895343244076300000044975713446446-elm">
-                                      <img alt="Vector7287" src="public/vector7287-lgce.svg"
-                                        className="joint3-thq-vector-elm7" />
-                                    </div>
-                                    <div className="joint3-thq-group-elm8">
-                                      <div className="joint3-thq-clippathgroup-elm3">
-                                        <div className="joint3-thq-svgid000001817935301781070939000000160947296855471-elm">
-                                          <img alt="Vector7287" src="public/vector7287-tqho.svg"
-                                            className="joint3-thq-vector-elm8" />
-                                        </div>
-
-                                        <div className="joint3-thq-group-elm9">
-                                          {/* <img alt="Rectangle7287" src="public/Max_logo-32x32.png"
-                                            className="joint3-thq-rectangle-elm" /> */}
-                                          <img src="https://maxicons.ru/icons/Max_logo.svg" alt="MAX" width={32} height={32} />
-                                        </div>
-
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        
-                        </div>
+      <div className="joint3-thq-logostelegram-elm2">
+        <img alt="Vector7286" src="public/vector7286-15ru.svg" className="joint3-thq-vector-elm5" />
+        <div className="joint3-thq-maxmessengerhorizontallogo1-elm">
+          <div className="joint3-thq-group-elm6">
+            <div className="joint3-thq-clippathgroup-elm1">
+              <div className="joint3-thq-group-elm7">
+                <div className="joint3-thq-clippathgroup-elm2">
+                  <div className="joint3-thq-svgid000000275732895343244076300000044975713446446-elm">
+                  </div>
+                  <div className="joint3-thq-clippathgroup-elm3">
+                    <div className="joint3-thq-group-elm9">
+                      <img src="https://maxicons.ru/icons/Max_logo.svg" alt="MAX" width={32} height={32} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
