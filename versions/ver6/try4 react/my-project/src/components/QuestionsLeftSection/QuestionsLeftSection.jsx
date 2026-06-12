a0594ce69a19b2e9b24bc4e8dff501b465f537ba
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-12 19:02:04 +0300

    removed unnecessary commented code

--- versions/ver6/try4 react/my-project/src/components/QuestionsLeftSection/QuestionsLeftSection.jsx
+++ versions/ver6/try4 react/my-project/src/components/QuestionsLeftSection/QuestionsLeftSection.jsx
@@ -22,51 +22,6 @@ export function QuestionsLeftSection({ }) {
 
                             <MaxLogo />
 
-                            {
-                                /* <div className="joint3-thq-logostelegram-elm2">
-                                 <img alt="Vector7286" src="public/vector7286-15ru.svg" className="joint3-thq-vector-elm5" />
-                                 <div className="joint3-thq-maxmessengerhorizontallogo1-elm">
-                                   <div className="joint3-thq-group-elm6">
-                                     <div className="joint3-thq-clippathgroup-elm1">
-                                       <div className="joint3-thq-svgid000000412575222465570809300000162176433937619-elm">
-                                         <img alt="Vector7287" src="public/vector7287-vj3r.svg" className="joint3-thq-vector-elm6" />
-                                       </div>
-                                       <div className="joint3-thq-group-elm7">
-                                         <div className="joint3-thq-clippathgroup-elm2">
-                                           <div className="joint3-thq-svgid000000275732895343244076300000044975713446446-elm">
-                                             <img alt="Vector7287" src="public/vector7287-lgce.svg"
-                                               className="joint3-thq-vector-elm7" />
-                                           </div>
-                                           <div className="joint3-thq-group-elm8">
-                                             <div className="joint3-thq-clippathgroup-elm3">
-                                               <div className="joint3-thq-svgid000001817935301781070939000000160947296855471-elm">
-                                                 <img alt="Vector7287" src="public/vector7287-tqho.svg"
-                                                   className="joint3-thq-vector-elm8" />
-                                               </div>
-                                                 <div className="joint3-thq-group-elm9">
-                                                 
-                                                 <img src="https://maxicons.ru/icons/Max_logo.svg" alt="MAX" width={32} height={32} />
-                                               </div>
-                                               </div>
-                                           </div>
-                                         </div>
-                                       </div>
-                                     </div>
-                                   </div>
-                                 </div>
-                                </div> */
-                            }
-
-
-
-
-
-
-
-
-
-
-
                             <img alt="logostelegram7286" src="public/logostelegram7286-26se.svg" className="joint3-thq-logostelegram-elm3" />
                             <img alt="logostelegram7287" src="public/logostelegram7287-0rp.svg" className="joint3-thq-logostelegram-elm4" />
                         </div>
@@ -119,22 +74,7 @@ export function QuestionsLeftSection({ }) {
 
 
                     <ActionButtonBlueMediumText text='Оставить заявку'> </ActionButtonBlueMediumText>
-                    {
-                        /* <div className="joint3-thq-component4-elm2">
-                         <div className="joint3-thq-frame1060-elm9"></div>
-                         <div className="joint3-thq-frame5-elm8">
-                           <span className="joint3-thq-text-elm202">
-                             СИНЯЯ Оставить заявку
-                           </span>
-                         </div>
-                         <div className="joint3-thq-frame1059-elm8">
-                           <img alt="Frame1061I728" src="public/frame1061i728-f6ge.svg" className="joint3-thq-frame1061-elm8" />
-                         </div>
-                        </div> */
-                    }
-
-
-
+                   
                 </div>
             </div>
         </div>
