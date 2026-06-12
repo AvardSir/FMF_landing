7bc0759fb388fb54807f349ef72e241ac6e4d828
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-12 18:54:35 +0300

    questions section in a separate component

--- versions/ver6/try4 react/my-project/src/App.jsx
+++ versions/ver6/try4 react/my-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { QuestionsLeftSection } from './components/QuestionsLeftSection/QuestionsLeftSection'
 
 import ActionButtonWhiteBigText from './components/hoverButtons/ActionButtonWhiteBigText/ActionButtonWhiteBigText'
 import BlueSmallChekcbox from './components/BlueSmallChekcbox/BlueSmallChekcbox'
@@ -10,7 +11,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import ActionButtonBlueMediumText from './components/hoverButtons/ActionButtonBlueMediumText/ActionButtonBlueMediumText '
 import StikyJar from './components/stikyJar/stikyJar'
-import MaxLogo from './components/MaxLogo/MaxLogo'
+// import MaxLogo from './components/MaxLogo/MaxLogo'
 import LeftRightArrow from './components/LeftRightArrow/LeftRightArrow'
 
 
@@ -301,30 +302,7 @@ function App() {
                   </div>
 
 
-                  {/* <ActionButtonWhiteBigText /> */}
-
-                  {/* <ActionButtonBlueMediumText></ActionButtonBlueMediumText> */}
-                  {/* ActionButtonBlueMediumText */}
                   <ActionButtonBlueMediumText />
-
-
-                  {/* <div className="joint3-thq-frame1075-elm">
-                    <div className="joint3-thq-component2-elm1">
-                      <div className="joint3-thq-frame1060-elm2">
-                        <div className="joint3-thq-tablerbasket-elm2">
-                          <div className="joint3-thq-group-elm2"></div>
-                        </div>
-                      </div>
-                      <div className="joint3-thq-frame5-elm2">
-                        <span className="joint3-thq-text-elm149">
-                          СИНЯЯ Купить в 1 клик
-                        </span>
-                      </div>
-                      <div className="joint3-thq-frame1059-elm2">
-                        <img alt="Frame1061I676" src="public/frame1061i676-cigp.svg" className="joint3-thq-frame1061-elm2" />
-                      </div>
-                    </div>
-                  </div> */}
 
 
 
@@ -641,147 +619,19 @@ function App() {
 
 
             </div>
-            <div className="joint3-thq-group5-elm">
-              <div className="joint3-thq-frame1119-elm">
-                <div className="joint3-thq-frame55-elm">
-                  <span className="joint3-thq-text-elm189">
-                    Остались вопросы? Напишите нам
-                  </span>
-                </div>
-                <div className="joint3-thq-frame1118-elm">
-                  <div className="joint3-thq-frame1115-elm">
-                    <div className="joint3-thq-frame64-elm2">
-                      <span className="joint3-thq-text-elm190">Эл. почта</span>
-                      <span className="joint3-thq-text-elm191">info@test.ru</span>
-                    </div>
-                    <div className="joint3-thq-frame70-elm">
-                      <span className="joint3-thq-text-elm192">Мессенджеры</span>
-                      <div className="joint3-thq-frame71-elm">
-                        <img alt="logostelegram7286" src="public/logostelegram7286-9tf.svg"
-                          className="joint3-thq-logostelegram-elm1" />
 
-                        <MaxLogo />
 
-                        {/* <div className="joint3-thq-logostelegram-elm2">
-                          <img alt="Vector7286" src="public/vector7286-15ru.svg" className="joint3-thq-vector-elm5" />
-                          <div className="joint3-thq-maxmessengerhorizontallogo1-elm">
-                            <div className="joint3-thq-group-elm6">
-                              <div className="joint3-thq-clippathgroup-elm1">
-                                <div className="joint3-thq-svgid000000412575222465570809300000162176433937619-elm">
-                                  <img alt="Vector7287" src="public/vector7287-vj3r.svg" className="joint3-thq-vector-elm6" />
-                                </div>
-                                <div className="joint3-thq-group-elm7">
-                                  <div className="joint3-thq-clippathgroup-elm2">
-                                    <div className="joint3-thq-svgid000000275732895343244076300000044975713446446-elm">
-                                      <img alt="Vector7287" src="public/vector7287-lgce.svg"
-                                        className="joint3-thq-vector-elm7" />
-                                    </div>
-                                    <div className="joint3-thq-group-elm8">
-                                      <div className="joint3-thq-clippathgroup-elm3">
-                                        <div className="joint3-thq-svgid000001817935301781070939000000160947296855471-elm">
-                                          <img alt="Vector7287" src="public/vector7287-tqho.svg"
-                                            className="joint3-thq-vector-elm8" />
-                                        </div>
 
-                                        <div className="joint3-thq-group-elm9">
-                                          
-                                          <img src="https://maxicons.ru/icons/Max_logo.svg" alt="MAX" width={32} height={32} />
-                                        </div>
 
-                                      </div>
-                                    </div>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div> */}
+            <QuestionsLeftSection     />
+            {/*  */}
 
 
 
 
 
-
-
-
-
-
-
-                        <img alt="logostelegram7286" src="public/logostelegram7286-26se.svg"
-                          className="joint3-thq-logostelegram-elm3" />
-                        <img alt="logostelegram7287" src="public/logostelegram7287-0rp.svg"
-                          className="joint3-thq-logostelegram-elm4" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="joint3-thq-frame1117-elm">
-                    <div className="joint3-thq-frame1116-elm">
-                      <div className="joint3-thq-frame46-elm">
-                        <div className="joint3-thq-frame48-elm1">
-                          <span className="joint3-thq-text-elm193">Имя</span>
-                          <div className="joint3-thq-frame48-elm2">
-                            <div className="joint3-thq-frame49-elm1">
-                              <span className="joint3-thq-text-elm194">
-                                Как к вам обращаться
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="joint3-thq-frame47-elm">
-                          <span className="joint3-thq-text-elm195">Телефон</span>
-                          <div className="joint3-thq-frame48-elm3">
-                            <div className="joint3-thq-frame49-elm2">
-                              <span className="joint3-thq-text-elm196">
-                                +7 (___) ___-__-__
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="joint3-thq-frame49-elm3">
-                          <span className="joint3-thq-text-elm197">Эл. почта</span>
-                          <div className="joint3-thq-frame48-elm4">
-                            <div className="joint3-thq-frame49-elm4">
-                              <span className="joint3-thq-text-elm198">
-                                Ваша эл.почта
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="joint3-thq-frame72-elm">
-                        <img alt="Rectangle117287" src="public/rectangle117287-xel-200h.png"
-                          className="joint3-thq-rectangle11-elm" />
-                        <span className="joint3-thq-text-elm199">
-                          <span className="joint3-thq-text-elm200">
-                            Я даю свое согласие на обработку персональных данных
-                          </span>
-                          
-                        </span>
-                      </div>
-                    </div>
-
-
-                    <ActionButtonBlueMediumText text='Оставить заявку'> </ActionButtonBlueMediumText>
-                    {/* <div className="joint3-thq-component4-elm2">
-                      <div className="joint3-thq-frame1060-elm9"></div>
-                      <div className="joint3-thq-frame5-elm8">
-                        <span className="joint3-thq-text-elm202">
-                          СИНЯЯ Оставить заявку
-                        </span>
-                      </div>
-                      <div className="joint3-thq-frame1059-elm8">
-                        <img alt="Frame1061I728" src="public/frame1061i728-f6ge.svg" className="joint3-thq-frame1061-elm8" />
-                      </div>
-                    </div> */}
-
-
-
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
-
+        
         </div>
         {/* <link rel="canonical" href="https://fmf-test-2d269j.teleporthq.site/" /> */}
       </div>
---
+++ versions/ver6/try4 react/my-project/src/components/QuestionsLeftSection/QuestionsLeftSection.jsx
@@ -0,0 +1,142 @@
+import React from "react";
+import MaxLogo from "../MaxLogo/MaxLogo";
+import ActionButtonBlueMediumText from "../hoverButtons/ActionButtonBlueMediumText/ActionButtonBlueMediumText ";
+export function QuestionsLeftSection({ }) {
+    return <div className="joint3-thq-group5-elm">
+        <div className="joint3-thq-frame1119-elm">
+            <div className="joint3-thq-frame55-elm">
+                <span className="joint3-thq-text-elm189">
+                    Остались вопросы? Напишите нам
+                </span>
+            </div>
+            <div className="joint3-thq-frame1118-elm">
+                <div className="joint3-thq-frame1115-elm">
+                    <div className="joint3-thq-frame64-elm2">
+                        <span className="joint3-thq-text-elm190">Эл. почта</span>
+                        <span className="joint3-thq-text-elm191">info@test.ru</span>
+                    </div>
+                    <div className="joint3-thq-frame70-elm">
+                        <span className="joint3-thq-text-elm192">Мессенджеры</span>
+                        <div className="joint3-thq-frame71-elm">
+                            <img alt="logostelegram7286" src="public/logostelegram7286-9tf.svg" className="joint3-thq-logostelegram-elm1" />
+
+                            <MaxLogo />
+
+                            {
+                                /* <div className="joint3-thq-logostelegram-elm2">
+                                 <img alt="Vector7286" src="public/vector7286-15ru.svg" className="joint3-thq-vector-elm5" />
+                                 <div className="joint3-thq-maxmessengerhorizontallogo1-elm">
+                                   <div className="joint3-thq-group-elm6">
+                                     <div className="joint3-thq-clippathgroup-elm1">
+                                       <div className="joint3-thq-svgid000000412575222465570809300000162176433937619-elm">
+                                         <img alt="Vector7287" src="public/vector7287-vj3r.svg" className="joint3-thq-vector-elm6" />
+                                       </div>
+                                       <div className="joint3-thq-group-elm7">
+                                         <div className="joint3-thq-clippathgroup-elm2">
+                                           <div className="joint3-thq-svgid000000275732895343244076300000044975713446446-elm">
+                                             <img alt="Vector7287" src="public/vector7287-lgce.svg"
+                                               className="joint3-thq-vector-elm7" />
+                                           </div>
+                                           <div className="joint3-thq-group-elm8">
+                                             <div className="joint3-thq-clippathgroup-elm3">
+                                               <div className="joint3-thq-svgid000001817935301781070939000000160947296855471-elm">
+                                                 <img alt="Vector7287" src="public/vector7287-tqho.svg"
+                                                   className="joint3-thq-vector-elm8" />
+                                               </div>
+                                                 <div className="joint3-thq-group-elm9">
+                                                 
+                                                 <img src="https://maxicons.ru/icons/Max_logo.svg" alt="MAX" width={32} height={32} />
+                                               </div>
+                                               </div>
+                                           </div>
+                                         </div>
+                                       </div>
+                                     </div>
+                                   </div>
+                                 </div>
+                                </div> */
+                            }
+
+
+
+
+
+
+
+
+
+
+
+                            <img alt="logostelegram7286" src="public/logostelegram7286-26se.svg" className="joint3-thq-logostelegram-elm3" />
+                            <img alt="logostelegram7287" src="public/logostelegram7287-0rp.svg" className="joint3-thq-logostelegram-elm4" />
+                        </div>
+                    </div>
+                </div>
+                <div className="joint3-thq-frame1117-elm">
+                    <div className="joint3-thq-frame1116-elm">
+                        <div className="joint3-thq-frame46-elm">
+                            <div className="joint3-thq-frame48-elm1">
+                                <span className="joint3-thq-text-elm193">Имя</span>
+                                <div className="joint3-thq-frame48-elm2">
+                                    <div className="joint3-thq-frame49-elm1">
+                                        <span className="joint3-thq-text-elm194">
+                                            Как к вам обращаться
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="joint3-thq-frame47-elm">
+                                <span className="joint3-thq-text-elm195">Телефон</span>
+                                <div className="joint3-thq-frame48-elm3">
+                                    <div className="joint3-thq-frame49-elm2">
+                                        <span className="joint3-thq-text-elm196">
+                                            +7 (___) ___-__-__
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="joint3-thq-frame49-elm3">
+                                <span className="joint3-thq-text-elm197">Эл. почта</span>
+                                <div className="joint3-thq-frame48-elm4">
+                                    <div className="joint3-thq-frame49-elm4">
+                                        <span className="joint3-thq-text-elm198">
+                                            Ваша эл.почта
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="joint3-thq-frame72-elm">
+                            <img alt="Rectangle117287" src="public/rectangle117287-xel-200h.png" className="joint3-thq-rectangle11-elm" />
+                            <span className="joint3-thq-text-elm199">
+                                <span className="joint3-thq-text-elm200">
+                                    Я даю свое согласие на обработку персональных данных
+                                </span>
+
+                            </span>
+                        </div>
+                    </div>
+
+
+                    <ActionButtonBlueMediumText text='Оставить заявку'> </ActionButtonBlueMediumText>
+                    {
+                        /* <div className="joint3-thq-component4-elm2">
+                         <div className="joint3-thq-frame1060-elm9"></div>
+                         <div className="joint3-thq-frame5-elm8">
+                           <span className="joint3-thq-text-elm202">
+                             СИНЯЯ Оставить заявку
+                           </span>
+                         </div>
+                         <div className="joint3-thq-frame1059-elm8">
+                           <img alt="Frame1061I728" src="public/frame1061i728-f6ge.svg" className="joint3-thq-frame1061-elm8" />
+                         </div>
+                        </div> */
+                    }
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>;
+}
