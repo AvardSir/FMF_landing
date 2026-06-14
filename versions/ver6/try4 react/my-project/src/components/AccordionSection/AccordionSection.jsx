311dddc3e328b9ff80ed62c7b7058b6c386b7b5d
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-14 13:41:56 +0300

    Accordion section CSS in a separate file

--- versions/ver6/try4 react/my-project/src/components/AccordionSection/AccordionSection.jsx
+++ versions/ver6/try4 react/my-project/src/components/AccordionSection/AccordionSection.jsx
@@ -2,6 +2,10 @@
 import BlueSmallChekcbox from './../BlueSmallChekcbox/BlueSmallChekcbox';
 // import { BlueSmallChekcbox } from 'BlueSmallChekcbox';
 
+// import "AccordionSectio.css"
+import "./AccordionSection.css"  // ← две точки
+
+
 const AccordionSection = () => {
   return (
     <>
@@ -33,6 +37,23 @@ const AccordionSection = () => {
             </span>
           </div>
           {/* ... остальной код ... */}
+
+          </div>
+          <div className="joint3-thq-frame762-elm">
+            <div className="joint3-thq-frame1051-elm3">
+              <span className="joint3-thq-text-elm159">
+                Почему в форме порошка, а не капсул?
+              </span>
+              <img alt="Frame386761" src="public/frame386761-uqwf.svg" className="joint3-thq-frame38-elm3" />
+            </div>
+          </div>
+          <div className="joint3-thq-frame763-elm">
+            <div className="joint3-thq-frame1051-elm4">
+              <span className="joint3-thq-text-elm160">
+                Какие показания для приема комплекса
+              </span>
+              <img alt="Frame386761" src="public/frame386761-47n.svg" className="joint3-thq-frame38-elm4" />
+            </div>
         </div>
       </div>
     </>
