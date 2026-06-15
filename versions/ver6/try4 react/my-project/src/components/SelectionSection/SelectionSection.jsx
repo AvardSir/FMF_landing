2ccee2ccc7bf2db19f9660174849f615e7e094a5
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-15 13:48:58 +0300

    The selection component blanks all work

--- versions/ver6/try4 react/my-project/src/components/SelectionSection/SelectionSection.jsx
+++ versions/ver6/try4 react/my-project/src/components/SelectionSection/SelectionSection.jsx
@@ -3,54 +3,64 @@ import React from 'react';
 // import LeftRightArrow from './LeftRightArrow';
 import { LeftRightArrow } from './../LeftRightArrow/LeftRightArrow';
 import './SelectionSection.css'
+import { UnderSelectionPart } from './UnderSelectionPart/UnderSelectionPart';
+import { WhiteSelection } from './WhiteSelection/WhiteSelection';
 
 const SelectionSection = () => {
-  return (
-    <div className="joint3-thq-frame1076-elm">
-        
-      <section id="for-whom"></section>
-      <img
-        alt="Rectangle356761"
-        src="public/rectangle356761-jsri-900h.png"
-        className="joint3-thq-rectangle35-elm"
-      />
-      
-      <img
-        alt="Vector466761"
-        src="public/vector466761-x0yv.svg"
-        className="joint3-thq-vector46-elm"
-      />
-      <span className="joint3-thq-text-elm114">
-        
-        Подходит для всех, кто заботиться о своем здоровье
-        и думает о своем будущем заранее.
-      </span>
-      <div className="joint3-thq-frame277130179-elm">
-        <LeftRightArrow />
-        
-        <div className="joint3-thq-frame277130178-elm">
-          <div className="joint3-thq-frame277130175-elm">
-            <span className="joint3-thq-text-elm115">18+ лет</span>
-            <span className="joint3-thq-text-elm116">
-              Поддержка суставов при травмах
+    return (
+        <div className="joint3-thq-frame1076-elm">
+
+            <section id="for-whom"></section>
+            <img
+                alt="Rectangle356761"
+                src="public/rectangle356761-jsri-900h.png"
+                className="joint3-thq-rectangle35-elm"
+            />
+
+            <img
+                alt="Vector466761"
+                src="public/vector466761-x0yv.svg"
+                className="joint3-thq-vector46-elm"
+            />
+            <span className="joint3-thq-text-elm114">
+
+                Подходит для всех, кто заботиться о своем здоровье
+                и думает о своем будущем заранее.
             </span>
-          </div>
-          <div className="joint3-thq-frame277130176-elm">
-            <span className="joint3-thq-text-elm117">35+ лет</span>
-            <span className="joint3-thq-text-elm118">
-              Профилактика возрастных изменений
-            </span>
-          </div>
-          <div className="joint3-thq-frame277130177-elm">
-            <span className="joint3-thq-text-elm119">45+</span>
-            <span className="joint3-thq-text-elm120">
-              Сохранение здоровья у женщин
-            </span>
-          </div>
+            <div className="joint3-thq-frame277130179-elm">
+                <LeftRightArrow />
+
+                <div className="joint3-thq-frame277130178-elm">
+                    <UnderSelectionPart age="18+ лет" text="Поддержка суставов при травмах" />
+        
+                    {/* <div className="joint3-thq-frame277130175-elm">
+                        <span className="joint3-thq-text-elm115">18+ лет</span>
+                        <span className="joint3-thq-text-elm116">
+                            Поддержка суставов при травмах
+                        </span>
+                    </div> */}
+
+                    {/* WhiteSelection */}
+                    <WhiteSelection age="35+ лет" text="Профилактика возрастных изменений" />
+
+                    {/* <div className="joint3-thq-frame277130176-elm">
+                        <span className="joint3-thq-text-elm117">35+ лет</span>
+                        <span className="joint3-thq-text-elm118">
+                            Профилактика возрастных изменений
+                        </span>
+                    </div> */}
+
+                    
+                    <div className="joint3-thq-frame277130177-elm">
+                        <span className="joint3-thq-text-elm119">45+</span>
+                        <span className="joint3-thq-text-elm120">
+                            Сохранение здоровья у женщин
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SelectionSection;
