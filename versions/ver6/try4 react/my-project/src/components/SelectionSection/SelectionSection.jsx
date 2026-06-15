6cb83ee4fafdc936fddf38be78634ae605b3bf68
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-15 13:40:22 +0300

    I divide the component into subcomponents and everything works.

--- versions/ver6/try4 react/my-project/src/components/SelectionSection/SelectionSection.jsx
+++ versions/ver6/try4 react/my-project/src/components/SelectionSection/SelectionSection.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 // Предполагается, что LeftRightArrow — это отдельный компонент, который нужно импортировать
 // import LeftRightArrow from './LeftRightArrow';
 import { LeftRightArrow } from './../LeftRightArrow/LeftRightArrow';
+import './SelectionSection.css'
 
 const SelectionSection = () => {
   return (
     <div className="joint3-thq-frame1076-elm">
+        
       <section id="for-whom"></section>
       <img
         alt="Rectangle356761"
         src="public/rectangle356761-jsri-900h.png"
         className="joint3-thq-rectangle35-elm"
       />
+      
       <img
         alt="Vector466761"
         src="public/vector466761-x0yv.svg"
         className="joint3-thq-vector46-elm"
       />
       <span className="joint3-thq-text-elm114">
+        
         Подходит для всех, кто заботиться о своем здоровье
         и думает о своем будущем заранее.
       </span>
