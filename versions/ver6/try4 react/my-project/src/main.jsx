7a5c659a70ec1ae4b58ffbfd2b4de550eae4b73d
AvardSir <114823780+AvardSir@users.noreply.github.com>
2026-06-17 21:38:58 +0300

    Design work was done to integrate Wp cms into React

--- versions/ver6/try4 react/my-project/src/main.jsx
+++ versions/ver6/try4 react/my-project/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CmsDataProvider } from './context/CmsDataContext' // добавить
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+    <CmsDataProvider pageId={12}>   {/* обернуть */}
+      <App />
+    </CmsDataProvider>
   </StrictMode>,
 )
